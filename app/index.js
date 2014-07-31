4a146f81f8e9c4a52239969e2ab43595eef9187c
Chyld Medford <chyld.medford@gmail.com>
2014-07-31 12:53:46 -0500

    Added Jade Page

---
+++ app/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/static'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', function(req, res){
+  res.render('home');
+});
+
+app.get('/calc', function(req, res){
+  res.render('calc');
+});
+
+app.post('/calc', function(req, res){
+  var x = req.body.x * 1; 
+  var y = req.body.y * 1;
+  var ans;
+
+    switch(req.body.op){
+      case '+':
+        ans = x + y;
+        break;
+      case '-':
+        ans = x - y;
+        break;
+      case '*':
+        ans = x * y;
+        break;
+      case '/':
+        ans = x / y;
+     }
+
+  res.render('calc', {x:x, y:y, op:req.body.op, ans:ans});
+});
+
+var port = process.env.PORT;
+
+app.listen(port, function(){
+  console.log('Express is listening...')
+});
